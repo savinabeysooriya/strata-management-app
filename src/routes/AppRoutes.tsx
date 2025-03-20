1219edd84bb2e9bf372b9b56aa12f8160ebe954f
Savin Abeysooriya <savina@okloworld.com>
2025-03-20 22:45:06 +0530

    update

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
-import { AdminDashboard } from '../components/dashboard/AdminDashboard';
-import { BuildingList } from '../components/dashboard/BuildingList';
-// import { MaintenanceRequestsList } from '../components/dashboard/MaintenanceRequestsList';
-// import { TenantView } from '../components/dashboard/TenantView';
-// import { NewRequestForm } from '../components/dashboard/MaintenanceRequestsList';
+import { AdminDashboard } from '../components/dashboards/AdminDashboard';
+import { BuildingList } from '../components/dashboards/BuildingList';
+import { MaintenanceRequestsList } from '../components/dashboards/MaintenanceRequestsList';
+import { TenantView } from '../components/dashboards/TenantView';
+import { NewRequestForm } from '../components/dashboards/NewRequestForm';
 
 export const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<AdminDashboard />} />
     <Route path="/buildings" element={<BuildingList />} />
-    {/* <Route path="/requests" element={<MaintenanceRequestsList />} />
+    <Route path="/requests" element={<MaintenanceRequestsList />} />
     <Route path="/my-building" element={<TenantView />} />
-    <Route path="/new-request" element={<NewRequestForm />} /> */}
+    <Route path="/new-request" element={<NewRequestForm />} />
   </Routes>
 );
